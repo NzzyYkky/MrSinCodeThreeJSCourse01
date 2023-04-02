063d1b984fc3f3f90f0d761de01384ffd7b24618
NzzyYkky <62602398+NzzyYkky@users.noreply.github.com>
2023-04-02 22:37:17 +0900

    add material mesh position light

--- main.js
+++ main.js
@@ -30,6 +30,35 @@ const renderer = new THREE.WebGLRenderer({
 	alpha: true,
 });
 
+// Material
+const material = new THREE.MeshPhysicalMaterial({
+	color: '#3c94d7',
+	metalness: 0.86,
+	roughness: 0.37,
+	flatShading: true,
+});
+
+// Mesh
+const mesh1 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.4, 16, 60), material);
+const mesh2 = new THREE.Mesh(new THREE.OctahedronGeometry(), material);
+const mesh3 = new THREE.Mesh(
+	new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
+	material
+);
+const mesh4 = new THREE.Mesh(new THREE.IcosahedronGeometry(), material);
+
+// position
+mesh1.position.set(2, 0, 0);
+mesh2.position.set(-1, 0, 0);
+mesh3.position.set(2, 0, -6);
+mesh4.position.set(5, 0, 0);
+scene.add(mesh1, mesh2, mesh3, mesh4);
+
+// light
+const directionalLight = new THREE.DirectionalLight('#ffffff', 4);
+directionalLight.position.set(0.5, 1, 0);
+scene.add(directionalLight);
+
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(window.devicePixelRatio);
 
